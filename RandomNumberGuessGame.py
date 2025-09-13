c3017a1fe3ed1b81ed078028642c7a2dd1653131
ScpWIZ <131114469+Gamerjig@users.noreply.github.com>
2025-09-12 20:18:34 -0700

    Update RandomNumberGuessGame.py

--- RandomNumberGuessGame.py
+++ RandomNumberGuessGame.py
@@ -4,20 +4,32 @@ import os
 def clear():
     os.system("cls")
 
-clear()
-print("Welcome to my Random number guessing game!")
-print("Made by Gamerjig!")
-print("Can be from 0 to 10")
-
-
-rn = str(rand.randint(0, 10))
-pa = str(input("Enter your guess: "))
-
-clear()
-
-if pa == rn:
-    print("You win!")
-else:
-    print("You lost..")
-
-print("You guessed "+ pa + " the number was " + rn)
+while True:
+    clear()
+    print("Welcome to my Random number guessing game!")
+    print("Made by Gamerjig!")
+    print("Can be from 0 to 10")
+    
+    
+    rn = str(rand.randint(0, 10))
+    pa = str(input("Enter your guess: "))
+    
+    clear()
+    
+    if pa == rn:
+        print("You win!")
+    else:
+        print("You lost..")
+    
+    print("You guessed "+ pa + " the number was " + rn)
+    print("Play again?")
+    print("1 - Yes")
+    print("2 - No")
+    again = str(input("Enter your choose: "))
+    if again == "1":
+        print("Restarting")
+    elif again == "2":
+        break
+    else:
+        print("1 for yes")
+        print("2 for no")
